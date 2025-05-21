bf6696da99b6535a29b7f0f3d0ecf7bbfec8cad8
FranciscoVenegas1 <139238501+FranciscoVenegas1@users.noreply.github.com>
2025-05-21 18:53:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Endpoint para productos
 app.get('/api/productos', (req, res) => {
   res.json([
     {
@@ -14,6 +13,18 @@ app.get('/api/productos', (req, res) => {
         {
           "Fecha": "2023-05-10T03:00:00.000Z",
           "Valor": 89090.99
+        }
+      ]
+    },
+    {
+      "Código del producto": "MAN-54321",
+      "Marca": "Stanley",
+      "Código": "STA-12345",
+      "Nombre": "Martillo de acero Stanley",
+      "Precio": [
+        {
+          "Fecha": "2023-05-15T03:00:00.000Z",
+          "Valor": 15200.50
         }
       ]
     }
